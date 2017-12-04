f423a21bd5897e9fc0b0ceda70029cead6c6b9eb
Luiz Assis <luizaaca@gmail.com>
2017-12-04 18:36:51 -0200

    Adding a query object framework to the infrastructure layer

---
+++ Project.Webstore.Infrastructure/Querying/CriteriaOperator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Webstore.Infrastructure.Querying
+{
+    public enum CriteriaOperator
+    {
+        Equal,
+        LesserThanOrEqual,
+        HigherThanOrEqual,
+        Lesser,
+        Higher,
+        NotApplicable
+    }
+}
--- Project.Webstore.Infrastructure/Querying/Query.cs
+++ Project.Webstore.Infrastructure/Querying/Query.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Project.Webstore.Infrastructure.Querying
 {
     public class Query
     {
+        private IList<Query> _subQueries = new List<Query>();
+        private IList<Criterion> _criterias = new List<Criterion>();
+        private IList<OrderByClause> _orderByClauses = new List<OrderByClause>();
+
+        public IEnumerable<Criterion> Criteria
+        {
+            get { return _criterias; }
+        }
+
+        public IEnumerable<Query> SubQueries
+        {
+            get { return _subQueries; }
+        }
+
+        public IEnumerable<OrderByClause> OrderByClauses
+        {
+            get { return _orderByClauses; }
+        }
+
+        public void AddSubQuery(Query subQuery)
+        {
+            _subQueries.Add(subQuery);
+        }
+
+        public void AddCriteria(Criterion criteria)
+        {
+            _criterias.Add(criteria);
+        }
+
+        public void AddOrderByClause(OrderByClause orderByClause)
+        {
+            _orderByClauses.Add(orderByClause);
+        }
+
+        public QueryOperator QueryOperator { get; set; }
+        public bool Not { get; set; }
     }
 }
